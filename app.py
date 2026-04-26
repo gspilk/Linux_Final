046cbf4d774b5a50ba0ee099d0c7b0e39a6da5b7
jberry1103 <jberry2@zagmail.gonzaga.edu>
2026-04-26 16:12:57 -0700

    updated the ci file

--- app.py
+++ app.py
@@ -1,3 +1,6 @@
+'''
+app.py is the module to run the backend and frontend locally.
+'''
 from flask import Flask, current_app, jsonify, request, render_template
 
 app = Flask(__name__)
@@ -5,18 +8,28 @@ app = Flask(__name__)
 app.config['task_list'] = []
 
 
+
 @app.route('/')
 def index():
+    '''
+    This endpoint renders the frontend.
+    '''
     return render_template('webpage.html')
 
 
 @app.route('/listTasks', methods=['GET'])
 def list_task():
+    '''
+    This endpoint lists all the current tasks.
+    '''
     return jsonify(app.config['task_list']), 200
 
 
 @app.route('/addTask', methods=['POST'])
 def add_task():
+    '''
+    This endpoint adds a task to current list.
+    '''
     task = request.json.get('task')
     current_app.config['task_list'].append(task)
     return jsonify({"message": f"Task {task} successfully added!"}), 200
@@ -24,22 +37,28 @@ def add_task():
 
 @app.route('/deleteTask', methods=['POST'])
 def delete_task():
+    '''
+    This endpoint deletes a task from the current list.
+    '''
     try:
         task_index = request.json.get('task_number')
         deleted_task = current_app.config['task_list'].pop(task_index)
         return jsonify({"message": f"Task {deleted_task} successfully deleted!"}), 200
-    except Exception:
-        return jsonify({"message": f"Task was not found!"}), 404
+    except (IndexError, KeyError):
+        return jsonify({"message": "Task was not found!"}), 404
 
 
 @app.route('/updateTask', methods=['POST'])
 def update_task():
+    '''
+    This endpoint updates a task from the current list.
+    '''
     try:
         task_index = request.json.get('task_number')
         new_task = request.json.get('new_task')
         current_app.config['task_list'][task_index] = new_task
         return jsonify({"message": f"Task successfully updated to {new_task}!"}), 200
-    except Exception:
+    except (IndexError, KeyError):
         return jsonify({"message": "Task was not found!"}), 404
 
 
--- test_app.py
+++ test_app.py
@@ -1,22 +1,34 @@
+'''
+test_app.py is the module tests the api endpoints in app.py
+'''
 import pytest
 from app import app
 
 
-@pytest.fixture
-def client():
+@pytest.fixture(name="client")
+def fixture_client():
+    '''
+    fixture_client is the fixture for client to store the info about app including task_list.
+    '''
     app.config['TESTING'] = True
     app.config['task_list'] = ["First Task"]
-    with app.test_client() as client:
-        yield client
+    with app.test_client() as api_client:
+        yield api_client
 
 
 def test_valid_list_task_endpoint(client):
+    '''
+    test_valid_list_task_endpoint is this tests ensures endpoint lists tasks. 
+    '''
     response = client.get('/listTasks')
     assert response.status_code == 200
     assert response.json == ["First Task"]
 
 
 def test_valid_add_task_endpoint(client):
+    '''
+    test_valid_add_task_endpoint is this tests ensures successfully adds task. 
+    '''
     response = client.post('/addTask', json={'task': 'Test Task'})
     assert response.status_code == 200
     assert response.json == {"message": "Task Test Task successfully added!"}
@@ -24,6 +36,9 @@ def test_valid_add_task_endpoint(client):
 
 
 def test_valid_delete_task_endpoint(client):
+    '''
+    test_valid_delete_task_endpoint is this tests ensures successfully able to delete a task.
+    '''
     response = client.post('/deleteTask', json={'task_number': 0})
     assert response.status_code == 200
     assert response.json == {
@@ -33,6 +48,10 @@ def test_valid_delete_task_endpoint(client):
 
 
 def test_invalid_delete_task_endpoint(client):
+    '''
+    test_invalid_delete_task_endpoint is this tests ensures
+      catching an attempt to delete a task that does not exist. 
+    '''
     response = client.post('/deleteTask', json={'task_number': 2})
     assert response.status_code == 404
     assert response.json == {
@@ -41,6 +60,9 @@ def test_invalid_delete_task_endpoint(client):
 
 
 def test_valid_update_task_endpoint(client):
+    '''
+    test_valid_update_task_endpoint is this tests ensures successfully able to delete a task.
+    '''
     response = client.post(
         '/updateTask', json={'task_number': 0, "new_task": "Updated Task"})
     assert response.status_code == 200
@@ -50,6 +72,10 @@ def test_valid_update_task_endpoint(client):
 
 
 def test_invalid_update_task_endpoint(client):
+    '''
+    test_valid_update_task_endpoint is this tests ensures catching an attempt 
+        to update a task that does not exist. 
+    '''
     response = client.post(
         '/deleteTask', json={'task_number': 2, "new_task": "Updated Task"})
     assert response.status_code == 404
